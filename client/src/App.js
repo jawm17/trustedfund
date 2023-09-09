c11b14d68168d81596d5f38b60df98cb9810784c
Envy Hose <envyhose@Envys-MacBook-Pro.local>
2023-09-09 17:37:02 -0500

    create page

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,14 @@ import { Router, Route, Switch } from 'react-router-dom';
 import NoMatchPage from "./pages/NoMatchPage";
 import Home from "./pages/Home/Home";
 import Account from "./pages/Account/Account";
+import Create from './pages/Create/Create';
 
 function App() {
   return (
     <Router history={history}>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/create" component={Create} />
         <PrivateRoute path="/account" component={Account} />
         <Route path="/" component={NoMatchPage} />
       </Switch>
---
+++ client/src/pages/Create/Create.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./createStyle.css";
+
+export default function Create() {
+
+    return (
+        <div>
+            create
+        </div>
+    );
+}
