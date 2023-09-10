0910b9516f014b2b49ffdb96a561c7aa109d0303
jonesliddy77 <jonesliddy80@yahoo.com>
2023-09-10 07:32:33 -0500

    font-end

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // Pages
 import NoMatchPage from "./pages/NoMatchPage";
 import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects/Projects";
 import Account from "./pages/Account/Account";
 import Create from './pages/Create/Create';
 
@@ -15,6 +16,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
         <Route exact path="/create" component={Create} />
         <PrivateRoute path="/account" component={Account} />
         <Route path="/" component={NoMatchPage} />
