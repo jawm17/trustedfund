898d498f2802ad61c6f1bcb6b18b0b0fe7326359
jonesliddy77 <jonesliddy80@yahoo.com>
2023-09-10 11:02:43 -0500

    nav fix

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // Pages
 import NoMatchPage from "./pages/NoMatchPage";
 import Home from "./pages/Home/Home";
-import Projects from "./pages/Projects/Projects";
 import Account from "./pages/Account/Account";
 import Create from './pages/Create/Create';
 import ProjectPage from './pages/ProjectPage/ProjectPage';
@@ -17,7 +16,6 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/projects" component={Projects} />
         <Route exact path="/create" component={Create} />
         <Route exact path="/project/:projectId" component={ProjectPage} />
         <PrivateRoute path="/account" component={Account} />
